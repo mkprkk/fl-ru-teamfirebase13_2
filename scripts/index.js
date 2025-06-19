42523a89cb46b6eb3af73e09a4881201880b9f28
Maxim Smirnov <seymj@mail.ru>
2025-06-19 17:14:34 +0300

    Navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,55 @@
 const footerBlockTemplate =
   document.querySelector("#form-footer-block").content;
-const safetyContainer = footerBlockTemplate
-  .querySelector(".form__safety-container")
-  .cloneNode(true);
-const navigationBlock = footerBlockTemplate
-  .querySelector(".form__navigation")
-  .cloneNode(true);
-const nextButton = navigationBlock.querySelector("#next-button").content;
-const requestButton = navigationBlock.querySelector("#request-button").content;
 
-const footerBlock = document.querySelector(".form__footer-block");
+let currentStepIdx = 0;
 
-const testElement = document.getElementById("test");
+const forms = Array.from(document.querySelectorAll("form"));
+forms.forEach((form, idx) => {
+  const footerBlock = form.querySelector(".form__footer-block");
+  if (idx === forms.length - 1) {
+    initFooterBlock(footerBlock, { isFinal: true });
+  } else {
+    initFooterBlock(footerBlock);
+  }
 
-function initFooterBlock() {
+  if (idx === 0) return;
+  form.classList.add("hidden");
+});
+
+function initFooterBlock(footerBlock, isFinal) {
+  const navigationBlock = footerBlockTemplate
+    .querySelector(".form__navigation")
+    .cloneNode(true);
+
+  const nextButton = navigationBlock.querySelector("#next-button").content;
+  const requestButton =
+    navigationBlock.querySelector("#request-button").content;
+
+  const safetyContainer = footerBlockTemplate
+    .querySelector(".form__safety-container")
+    .cloneNode(true);
+
   footerBlock.prepend(safetyContainer);
-
-  navigationBlock.append(nextButton);
   footerBlock.prepend(navigationBlock);
-}
+  if (isFinal) {
+    navigationBlock.append(requestButton);
+  } else {
+    navigationBlock.append(nextButton);
+  }
 
-initFooterBlock();
+  const nextStepHandler = () => nextStep(forms);
+  const prevStepHandler = () => prevStep(forms);
+
+  const buttons = Array.from(navigationBlock.querySelectorAll(".form__button"));
+  buttons.forEach((button) => {
+    if (button.id === "nextButton") {
+      button.addEventListener("click", nextStepHandler);
+    } else if (button.id === "prevButton") {
+      button.addEventListener("click", prevStepHandler);
+    } else if (button.id === "requestButton") {
+    }
+  });
+}
 
 // --------------------------------------------------------------------------------
 
@@ -82,10 +111,16 @@ test.addEventListener("click", openFieldHandler);
 
 // --------------------------------------------------------------------------------
 
-const forms = Array.from(document.querySelectorAll('form'));
-forms.forEach((form, idx) => {
-  if (idx === 0) return;
-  form.classList.add('hidden');
-})
-// for (let i = 1, i < )
-//  document.getElementById(`step-${}`))
+function nextStep(forms) {
+  if (currentStepIdx === forms.length - 1) return;
+  forms[currentStepIdx].classList.add("hidden");
+  currentStepIdx += 1;
+  forms[currentStepIdx].classList.remove("hidden");
+}
+
+function prevStep(forms) {
+  if (currentStepIdx === 0) return;
+  currentStepIdx -= 1;
+  forms[currentStepIdx + 1].classList.add("hidden");
+  forms[currentStepIdx].classList.remove("hidden");
+}
